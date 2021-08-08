e4253fa4b934de2ff48a60c0830e727d9956b16a
snatarajan <snatarajan@smartshift.com>
2021-08-08 20:12:57 +0530

    file upload completed

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import { Form, Button, Col, Container } from 'react-bootstrap';
 import { Formik } from 'formik';
 import * as yup from 'yup';
-import { validateImageType} from './common.js'
-const SUPPORTED_FORMATS = ["image/jpg", "image/jpeg", "image/gif", "image/png"];
 const schema = yup.object().shape({
   address: yup.string().required('address is required'),
-  file: yup.string().required('file is required'),
-  location: yup.string().required('location is required'),
+  file: yup.mixed().required('file is required').test('fileType', 'Unsupported File Format', function (value) {
+    if(value){
+	const SUPPORTED_FORMATS = ['application/pdf','image/jpg', 'image/jpeg', 'image/png'];
+    return SUPPORTED_FORMATS.includes(value.type)
+	}
+  }),
+	location: yup.string().required('location is required'),
 });
 
 
@@ -37,7 +40,8 @@ class Confirmation extends Component {
                 <Formik
       validationSchema={schema}
        onSubmit={(values, actions) => {
-		 this.props.nextStep();
+		   console.log(values);
+		  // this.props.nextStep();
        }}  
       initialValues={{
         address: '',
@@ -53,6 +57,7 @@ class Confirmation extends Component {
         touched,
         isValid,
         errors,
+		setFieldValue
       }) => (
                     <Form noValidate onSubmit={handleSubmit}>
                     <div className="row  d-flex justify-content-center" style={{ marginTop: '140px' }} >
@@ -61,7 +66,7 @@ class Confirmation extends Component {
                                 <h4 className="card-header">Seller Pannel Register</h4>
                                 <div className="card-body">
                                     <div className="mb-3">
-                                        <Form.Group as={Col} controlId="Address">
+                                        <Form.Group as={Col}>
                                             <Form.Label className="label">Address </Form.Label>
                                             <Form.Control
                                                 type="text"
@@ -78,12 +83,16 @@ class Confirmation extends Component {
                                     </div>
 
                                     <div className="mb-3">
-                                        <Form.Group as={Col} controlId="formFile" className="mb-3">
+                                        <Form.Group as={Col} className="mb-3">
                                             <Form.Label>Default file input example</Form.Label>
-                                            <Form.Control type="file" 
-											    name='file'
-											    value={values.file}
-												onChange={handleChange}
+                                            <Form.Control
+												id="file"
+												name="file"
+												type="file"
+												onChange={(event) => {
+												  setFieldValue("file", event.currentTarget.files[0]);
+												}}
+												onBlur={handleBlur}
 												isValid={touched.file && !errors.file}
 												isInvalid={touched.file && !!errors.file}
 											/>
@@ -94,7 +103,7 @@ class Confirmation extends Component {
                                     </div>
 
                                     <div className="mb-3">
-                                        <Form.Group as={Col} controlId="location">
+                                        <Form.Group as={Col}>
                                             <Form.Label className="label">Location</Form.Label>
                                             <Form.Control
                                                 type="text"
